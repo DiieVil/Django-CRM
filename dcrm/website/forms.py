1bb5603bbe66ed5f2e17f74b332f668356287c3e
DiieVil <diegovicentevillagrasa@gmail.com>
2023-08-22 18:24:28 +0200

    Base finalizada

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label = "", widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Direccion Mail'}))
+    email = forms.EmailField(label = "", widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'E-Mail'}))
     first_name = forms.CharField(label = "", max_length = 50, widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre'}))
     last_name = forms.CharField(label = "", max_length = 50, widget= forms.TextInput(attrs={'class':'form-control', 'placeholder':'Apellido'}))
     
@@ -15,16 +16,34 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'Nombre'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nombre de usuario'
         self.fields['username'].label = ''
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido. 50 caracteres o menos. Solo letras, digitos y @/./+/-/_.</small></span>'
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido. 50 carácteres o menos. Solo letras, dígitos y @/./+/-/_.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Contraseña'
         self.fields['password1'].label = ''
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirmación de Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+# Creacion del formulario de añadido de registros
+
+# Create Add Record Form
+class AddRecordForm(forms.ModelForm):
+	first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Nombre", "class":"form-control"}), label="")
+	last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Apellido", "class":"form-control"}), label="")
+	email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), label="")
+	phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Teléfono", "class":"form-control"}), label="")
+	address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Dirección", "class":"form-control"}), label="")
+	city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Ciudad", "class":"form-control"}), label="")
+	state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Provincia", "class":"form-control"}), label="")
+	zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Código Zip", "class":"form-control"}), label="")
+
+	class Meta:
+		model = Record
+		exclude = ("user",)
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    # Se obtienen todos los datos de records
+    records = Record.objects.all()
+    
     # Se comprueba si el usuario se ha logeado
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,9 +23,9 @@ def home(request):
         else:
             messages.success(request, 'Ha ocurrido un error, pruebe de nuevo')
             return redirect('home')
-            
+    # Si el usuario ya esta logueado
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 #def login_user(request):
 #    pass
@@ -31,4 +36,66 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "Te has registrado correctamente")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Se ve el record del usuario seleccionado
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "Tienes que estar logueado para ver esta pagina")
+		return redirect('home')
+
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Registro borrado correctamente")
+		return redirect('home')
+	else:
+		messages.success(request, "Tienes que estar registrado para hacer la acción")
+		return redirect('home')
+
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Registro añadido")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "Tienes que estar registrado")
+		return redirect('home')
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "El registro ha sido actualizado")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "Tienes que estar registrado para realizar la acción")
+		return redirect('home')
