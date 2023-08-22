a448e5e789bad20184909aca8bde936a5bfaa58d
DiieVil <diegovicentevillagrasa@gmail.com>
2023-08-22 18:28:17 +0200

    Base actualizada

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$fr9+jhx&v!+0*-+ye29-%c8fo7i#^15^b^n4&b^jxpmu__#l1'
+SECRET_KEY = '#####'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
